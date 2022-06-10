6491a1cf62bb373a57b4fb03c639721db7fc90d8
xnlyu <lyutissues@gmail.com>
2022-06-10 14:14:33 +0800

    work by 0610

---
+++ css challenges D2/Day2.js
@@ -0,0 +1,24 @@
+function myFunction(){
+	// document.getElementById("menu").classList.add('active');
+	document.getElementById("menu").classList.toggle('active');
+	document.getElementById("menu").classList.remove('noAnimation');
+}
+
+document.getElementById('menu').addEventListner('click', myFunction)
+
+// //抽出來 變數
+// document.getElementById('menu').addEventListner('click', function(){
+// 	myFunction()
+// })
+
+// document.getElementById('menu').addEventListner('click', function(){
+// 	// document.getElementById("menu").classList.add('active');
+// 	document.getElementById("menu").classList.toggle('active');
+// 	document.getElementById("menu").classList.remove('noAnimation');
+// })
+
+
+// classList是這個類別內
+// ById是讀到這個身分證
+// addEventListner是事件偵聽器
+// toggle是新增切換
